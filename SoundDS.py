5a51b3166539ffdee8b956c8ddbe2ad3f21712f6
MSI\Lloyd Acha <lloydacha@gmail.com>
2022-02-13 13:21:36 +0800

    Update v1.0

--- SoundDS.py
+++ SoundDS.py
@@ -6,10 +6,8 @@ class SoundDS:
     def __init__(self, df, data_path):
         self.df = df
         self.data_path = str(data_path)
-        self.durations = 3000
+        self.duration = 3000
         self.sr = 44100
-        self.channel = 2
-        self.shift_pct = 0.4
         self.hop_length = 512
         self.n_fft = 1024
         
@@ -18,15 +16,15 @@ class SoundDS:
     
     def __getitem__(self, idx):
         audio_file = os.path.join(self.data_path, os.path.normpath(self.df['relative_path'][idx]))
-     
         class_id = self.df.loc[idx, 'classID']
         
         aud = AudioUtils.open(audio_file)
+
         #Augmentations here
         #aud = (AudioUtils.stretch(aud[0], 0.84), aud[1])
 
         #Samples should be 3s
-        aud = AudioUtils.pad_trunc(aud, self.durations)
+        aud = AudioUtils.pad_trunc(aud, self.duration)
 
         #spec = AudioUtils.spectrogram(aud, hop_length=self.hop_length, n_fft=self.n_fft)
         #log_spec = AudioUtils.spectrogram(aud, hop_length=self.hop_length, n_fft=self.n_fft, log = True)
